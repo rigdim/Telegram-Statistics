de25529fa0b1339793bb05f48cb646cf6ded36a9
Igor Riutin <riutin.ig@gmail.com>
2024-02-20 08:26:36 +0800

    Start refactoring

--- telegram-parser.py
+++ telegram-parser.py
@@ -10,94 +10,59 @@ from datetime import datetime, timedelta
 from openpyxl.styles import Border, Side
 import xlsxwriter.utility as Utility
 
+# Using to convert object to dictionary.
 class DictElement:
     def to_dict(self):
         return vars(self)
 
 class User(DictElement):
-    def __init__(self, id, name, message, date):
+    def __init__(self, id, name):
         self.id = id
         if name is None:
             self.name = 'Удаленный пользователь'
         else:
             self.name = name
-        self.messages = message
-        if message:
-            self.messages_count = 1
-            self.messages_dates = [(date, 1)]
-        else:
-            self.messages_count = 0
-            self.messages_dates = []
-        self.regions_count = {}  # Nested dictionary to store the region ID and the number of mentions of both as a city and as a district.
+        self.messages = []
+        self.messages_count = 0
+        self.region_mentions = {}  # Nested dictionary to store the region ID and the number of mentions of both as a city and as a district.
         self.region = None
         self.membership = None
         self.user_type = None
 
     def add_message(self, message):
-        self.messages += '\n' + message
-
-    # Counts messages by date.
-    def add_dates(self, date):
-        result = next((messages_date for messages_date in self.messages_dates if messages_date[0] == date), None)
-        if result is None:
-            self.messages_dates.append((date, 1))
-        else:
-            date_index = self.messages_dates.index(result)
-            self.messages_dates[date_index] = (result[0], result[1] + 1)
+        self.messages.append(message)
+        self.messages_count += 1
 
     def add_region(self, region_id, region_type):
         # Add or update the count for the specified region and type.
-        region_dict = self.regions_count.get(region_id, {"city": 0, "district": 0})
+        region_dict = self.region_mentions.get(region_id, {"city": 0, "district": 0})
         if region_type is not None:
             region_dict[region_type] += 1
-        self.regions_count[region_id] = region_dict
+        self.region_mentions[region_id] = region_dict
 
-    def add_messages_count(self):
-        self.messages_count += 1
-
-    def display_info(self):
-        print(f"Name: {self.name}")
-        print(f"ID: {self.id}")
-        print(f"Message Count: {self.messages_count}")
-
-        # Sort dictionary descending.
-        sorted_regions = sort_dict(self.regions_count)
-        for key, value in sorted_regions.items():
-            if key is not None:
-                print(f"* {regions[key]['match'][0]} - city: {value.get('city')}, district: {value.get('district')}")
-
-        if self.region is None:
-            print("Region not found!")
-        else:
-            print(f"Region: {self.region}")
-
-        print(f"Dates: {self.messages_dates}")
-        print(f"Membership: {self.membership}")
-        print("-" * 10)
-
-    def get_first_region(self, ):
-        if self.regions_count is not None:
-            sorted_regions = sort_dict(self.regions_count)
+    def get_first_region(self):
+        if self.region_mentions is not None:
+            sorted_regions = sort_dict(self.region_mentions)
             for key, value in sorted_regions.items():
                 if key is not None:
                     return key, value
         return None, None
-    
+
     # Get an appropriate region display name based on 'city' and 'district' number of mentions.
     def set_region(self):
-        if self.regions_count is not None:
-            first_region, mentions = self.get_first_region()
+        if self.region_mentions is not None:
+            first_region, type_mentions = self.get_first_region()
             if first_region is not None:
-                city_count = mentions.get("city", 0)
-                district_count = mentions.get("district", 0)
+                city_count = type_mentions.get("city", 0)
+                district_count = type_mentions.get("district", 0)
                 if (city_count > district_count) and ('name_city' in regions[first_region]):
                     self.region = regions[first_region]['name_city']
                 elif ('name_district' in regions[first_region]):
                     self.region = regions[first_region]['name_district']
                 else:
-                    self.region = None 
+                    self.region = None
 
-    def get_last_dates_count(self, last_days):
+    def get_last_messages_count(self, last_days):
         if self.messages_dates is not None:
             messages_count = 0
             for date, count in reversed(self.messages_dates):
@@ -127,6 +92,26 @@ class User(DictElement):
                 self.user_type = UserType.OPERATOR.value
         return self.user_type
 
+    def display_info(self):
+        print(f"Name: {self.name}")
+        print(f"ID: {self.id}")
+        print(f"Message Count: {self.messages_count}")
+
+        # Sort dictionary descending.
+        sorted_regions = sort_dict(self.region_mentions)
+        for key, value in sorted_regions.items():
+            if key is not None:
+                print(f"* {regions[key]['match'][0]} - city: {value.get('city')}, district: {value.get('district')}")
+
+        if self.region is None:
+            print("Region not found!")
+        else:
+            print(f"Region: {self.region}")
+
+        print(f"Dates: {self.messages[0].date}, {self.messages[1].date}, ... {self.messages[-2].date}, {self.messages[-1].date}")
+        print(f"Membership: {self.membership}")
+        print("-" * 10)
+
             
 def add_user(users_list, name, id, message = None, date = None, region_id = None, region_type = None, membership = None):
     # Check if the user with the given user_id already exists.
@@ -136,7 +121,6 @@ def add_user(users_list, name, id, message = None, date = None, region_id = None
         existing_user.add_message(message)
         existing_user.add_region(region_id, region_type)
         existing_user.add_messages_count()
-        existing_user.add_dates(date)
         existing_user.membership = membership
     else:
         # User does not exist, create a new user and add to the list.
